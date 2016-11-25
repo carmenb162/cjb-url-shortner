df64905d29a31ecf92348634f9a8ce8338f47d57
Carmen Barron <carmen.barron@gmail.com>
2016-11-25 14:10:04 -0500

    create postgresql database

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20161125183004) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "original"
     t.string   "short"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "dyanmic_link"
     t.string   "dynamic_link"
   end
 

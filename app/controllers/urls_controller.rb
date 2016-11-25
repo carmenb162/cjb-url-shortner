64194ba4dd1f066983394ee589cb0c1fcc37d821
Carmen Barron <carmen.barron@gmail.com>
2016-11-25 14:26:44 -0500

    add link_to for navigation

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -20,27 +20,11 @@ class UrlsController < ApplicationController
 
     @url.save
     redirect_to urls_index_path
-
-
-    # if @url.save
-    #   redirect_to urls_index_path
-    # else
-    #   redirect_to urls_new_path, :flash => {:error => "couldn't do it!"}
-    # end
-
   end
 
   def traffic_cop
   where_to_go = Url.find(params[:id])
 
   redirect_to where_to_go.original
-
-
   end
-
-
 end
-
-
-
-#@url.short = request.protocol+request.host_with_port +"/" + @url.short

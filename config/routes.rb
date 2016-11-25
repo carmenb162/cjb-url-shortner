8711bb37c05815b76bdc9c15f3d32b7e3b42414d
Carmen Barron <carmen.barron@gmail.com>
2016-11-25 12:04:17 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'urls/index'
+
+  get 'urls/new'
+
+  get 'urls/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
